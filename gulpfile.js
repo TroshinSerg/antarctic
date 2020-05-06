ef044dc50217442de2fc8b84a1527c998241a8d7
TroshinSerg <serge-troshin@yandex.ru>
2020-05-06 16:03:13 +0300

    Исправляет ошибку в сборщике

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch('source/img/**/*.{png,jpg,svg,webp}', gulp.series('images', 'webp'));
+  gulp.watch('source/img/**/*.{png,jpg,svg,webp}', gulp.series('img', 'webp'));
 });
 
 gulp.task("refresh", function (done) {
