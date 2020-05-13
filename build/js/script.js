ee6eeffdf8c6e7d37b76aef625bc2a370fe49c73
TroshinSerg <serge-troshin@yandex.ru>
2020-05-13 18:33:16 +0300

    Устраняет баги по замечания тестировщика

--- build/js/script.js
+++ build/js/script.js
@@ -5,6 +5,8 @@ var toggle = header.querySelector('.toggle');
 var navigation = header.querySelector('.navigation');
 var logo = header.querySelector('.logo');
 var anchors = document.querySelectorAll('a.js-scroll-to');
+var phoneField = document.querySelector('.js-phone');
+var regex = /^-?\d*$/;
 var ClassName = {
   TOGGLE_OPENED: 'toggle--opened',
   NAV_SHOWN: 'navigation--shown',
@@ -17,6 +19,10 @@ anchors.forEach(function (item) {
 
 document.querySelector('html').classList.remove('no-js');
 toggle.addEventListener('click', onToggleClick);
+
+setInputFilter(phoneField, function(value) {
+  return regex.test(value);
+});
 
 function onToggleClick() {
   toggle.classList.toggle(ClassName.TOGGLE_OPENED);
@@ -33,4 +39,21 @@ function onAnchorClick(evt) {
     block: 'start'
   });
   evt.currentTarget.blur();
+}
+
+function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    textbox.addEventListener(event, function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  });
 }
